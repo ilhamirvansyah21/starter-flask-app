a245cf2066e0a89112bcca39ad11a21d36619e69
unknown <ilhamirvansyah03@gmail.com>
2020-12-07 21:52:33 +0700

    first commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'title one',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat ea quos inventore qui molestiae optio dicta consequuntur suscipit exercitationem soluta, reprehenderit rem alias necessitatibus eum corrupti architecto deleniti possimus.',
+            'author': 'Ilham Irvansyah',
+            'create_date': '12-11-2020'
+        },
+        {
+            'id': 2,
+            'title': 'title two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat ea quos inventore qui molestiae optio dicta consequuntur suscipit exercitationem soluta, reprehenderit rem alias necessitatibus eum corrupti architecto deleniti possimus.',
+            'author': 'John Doe',
+            'create_date': '12-11-2020'
+        },
+        {
+            'id': 3,
+            'title': 'title three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat ea quos inventore qui molestiae optio dicta consequuntur suscipit exercitationem soluta, reprehenderit rem alias necessitatibus eum corrupti architecto deleniti possimus.',
+            'author': 'Thomas',
+            'create_date': '12-11-2020'
+        }
+    ]
+
+    return articles
